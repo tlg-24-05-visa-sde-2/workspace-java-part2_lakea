6428aaf5502aa788eb187f62ed0f6b15c2b029c2
Lakea <lakeag@tlgcohort.com>
2024-07-25 17:11:27 -0400

    Lab06.1 Tuner as true inner class display type as comparator as static nested classes i.e, Television.DisplayType

--- Lab06.1/src/com/entertainment/Television.java
+++ Lab06.1/src/com/entertainment/Television.java
@@ -8,6 +8,7 @@
  */
 package com.entertainment;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 public class Television implements Comparable<Television> {
@@ -19,7 +20,9 @@ public class Television implements Comparable<Television> {
     private String brand;
     private int volume;
     private DisplayType display;
-    private Tuner tuner = new Tuner();  // set up internally and used for channel management
+
+    //Television HAS-A Tuner
+    private final Tuner tuner = new Tuner();  // set up internally and used for channel management
 
     public Television() {
     }
@@ -113,5 +116,48 @@ public class Television implements Comparable<Television> {
                      Objects.equals(this.getDisplay(), other.getDisplay());
         }
         return result;
+    }
+
+
+// NAMED, MEMBER-LEVEL INNER CLASSES
+    private class Tuner {
+        private int channel = 3;  // default channel for cable and satellite customers
+
+        public int getChannel() {
+            return this.channel;
+        }
+
+        public void setChannel(int channel) {
+            this.channel = channel;
+        }
+    }
+
+    // STATIC NESTED CLASSES
+    // outside here, this is referred to as Television.DisplayType
+    public static enum DisplayType {
+        LCD, LED, OLED, PLASMA, CRT
+    }
+
+    // outside here, this is referred to as Television.ChannelComparator
+    public static class ChannelComparator implements Comparator<Television> {
+
+        @Override
+        public int compare(Television tv1, Television tv2) {
+            return Integer.compare(tv1.getCurrentChannel(), tv2.getCurrentChannel());
+        }
+    }
+
+    // outside here, this is referred to as Television.BrandChannelComparator
+    public static class BrandChannelComparator implements Comparator<Television> {
+
+        @Override
+        public int compare(Television tv1, Television tv2) {
+            int result = tv1.getBrand().compareTo(tv2.getBrand());
+
+            if (result == 0) {
+                result = Integer.compare(tv1.getCurrentChannel(), tv2.getCurrentChannel());
+            }
+            return result;
+        }
     }
 }
